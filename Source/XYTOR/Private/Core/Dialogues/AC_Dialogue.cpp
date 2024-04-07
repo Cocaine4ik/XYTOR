9f19a8fec7496a79bb13033200f41d2ed8d16a46
Alex <kokotkin.alexander@gmail.com>
2024-04-08 00:06:42 +0300

    Create Dialogue Component for AI Pawns

---
+++ Source/XYTOR/Private/Core/Dialogues/AC_Dialogue.cpp
@@ -0,0 +1,13 @@
+// XYTOR. All Rights Reserved.
+
+
+#include "Core/Dialogues/AC_Dialogue.h"
+
+// Sets default values for this component's properties
+UAC_Dialogue::UAC_Dialogue()
+{
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = false;
+
+}
