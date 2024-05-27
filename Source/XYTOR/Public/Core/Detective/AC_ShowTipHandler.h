2d1505eaa7fbb29581ac14797aecd34e5e8d5b7a
aferprog <68941824+aferprog@users.noreply.github.com>
2024-05-27 16:18:26 +0200

    Base files for tips displaying

---
+++ Source/XYTOR/Public/Core/Detective/AC_ShowTipHandler.h
@@ -0,0 +1,17 @@
+// XYTOR. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/Interaction/AC_InteractionHandler.h"
+#include "AC_ShowTipHandler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XYTOR_API UAC_ShowTipHandler : public UAC_InteractionHandler
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/XYTOR/Public/Core/Detective/W_TipsBase.h
@@ -0,0 +1,17 @@
+// XYTOR. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/WidgetManager/Widgets/W_BackgroundBase.h"
+#include "W_TipsBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XYTOR_API UW_TipsBase : public UW_BackgroundBase
+{
+	GENERATED_BODY()
+	
+};
